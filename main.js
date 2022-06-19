f93583cd8c18879f0109630105599ca829f54ee1
ashfak101 <ashfakulkarim2@gmail.com>
2022-06-20 00:08:34 +0600

    Navber Responsive issue fixed

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+$(document).ready(function () {
+    $('.slider').owlCarousel({
+      loop: true,
+      nav: true,
+      navText: ['<i class="icon fa-solid fa-arrow-left"></i>', '<i class="icon fa-solid fa-arrow-right"></i>'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        800: {
+          items: 2
+        },
+        1000: {
+          items: 4
+        }
+      }
 
+    });
 
-console.log('lik');
+    $('#sliders').owlCarousel({
+      loop: true,
+      nav: true,
+      dots: true,
+      navText: ['<i', ''],
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+
+    })
+  });
+
+  const tab = document.querySelectorAll('.btn-tab');
+console.log(tab);
+for(let i=0 ; i<tab.length; i++){
+    tab[0].style.borderBottom ='5px solid #091c30';
+}
+
+for(let i=0 ; i<tab.length; i++){
+    tab[i].addEventListener('click', function(){
+        for(let i=0 ; i<tab.length; i++){
+            tab[i].style.borderBottom ='none';
+        }
+        this.style.borderBottom ='5px solid #091c30';
+    })
+}
